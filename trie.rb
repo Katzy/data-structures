0efd85adb04c79e82e31fb84a62dcf4c61cf7a78
Scott <scottalankatz@gmail.com>
2015-04-26 20:57:48 -0400

    Built Functional Trie using a Tree & LinkedList

--- trie.rb
+++ trie.rb
@@ -1,10 +1,73 @@
 require_relative "tree"
 
-# Implement a Trie (sometimes called a "prefix tree")
-# See: http://en.wikipedia.org/wiki/Trie
-#
-# A Trie allows for efficient prefix searching, i.e.,
-# "Find me all things that start with 'Foo'"
+class Tree
+
+  def empty?
+    self.value.nil?
+  end
+end
 
 class Trie
+  attr_reader :root
+
+  def children
+    self.root.children || self.children
+  end
+
+  attr_accessor :prefix
+
+  def initialize
+    @root = Tree.new(nil)
+    @prefix = []
+  end
+
+  def find(item)
+    tree_node = self.root.children.head
+    temp_node = nil
+
+    prefix = []
+
+    until tree_node.value.nil?
+      if tree_node.value.value == item[0]
+        prefix.push(item[0])
+        temp_node = tree_node
+        item = item[1..-1]
+        tree_node = tree_node.value.children.head
+      else
+        tree_node = tree_node.next
+      end
+    end
+
+      @prefix = prefix
+      temp_node
+  end
+
+  def insert(item)
+
+    prefix = []
+
+    if find(item) == nil
+      temp = self.root
+      until item.length == 0
+        temp.add_child(item[0])
+        prefix.push(item[0])
+        item = item[1..-1]
+        temp = temp.children.head.value
+      end
+      p prefix
+    else
+      temp_node = find(item).value
+      times = item.length - self.prefix.length  # difference between item string and prefix
+      item = item[-times, times]
+      until item.length == 0
+        temp_node.add_child(item[0])
+        prefix.push(item[0])
+        item = item[1..-1]
+        temp_node = temp_node.children.head.value
+      end
+      p prefix
+    end
+    self
+  end
 end
+
