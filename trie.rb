22b18de8be421b37d1de1ba72fff7a846c4f6fec
Scott <scottalankatz@gmail.com>
2015-04-27 08:56:44 -0400

    Refactored Trie

--- trie.rb
+++ trie.rb
@@ -1,23 +1,16 @@
 require_relative "tree"
 
 class Trie
-  attr_reader :root
-
-  def children
-    self.root.children || self.children
-  end
-
-  attr_accessor :prefix
+  attr_reader :root, :prefix
 
   def initialize
     @root = Tree.new(nil)
-    @prefix = []
+    @prefix = []    # keeping track of characters of prefix
   end
 
   def find(item)
     tree_node = self.root.children.head
     temp_node = nil
-
     prefix = []
 
     until tree_node.value.nil?
@@ -40,26 +33,20 @@ class Trie
     prefix = []
 
     if find(item) == nil
-      temp = self.root
-      until item.length == 0
-        temp.add_child(item[0])
-        prefix.push(item[0])
-        item = item[1..-1]
-        temp = temp.children.head.value
-      end
-      p prefix
+      temp_node = self.root
     else
       temp_node = find(item).value
-      times = item.length - self.prefix.length  # difference between item string and prefix
-      item = item[-times, times]
-      until item.length == 0
-        temp_node.add_child(item[0])
-        prefix.push(item[0])
-        item = item[1..-1]
-        temp_node = temp_node.children.head.value
-      end
-      p prefix
+      item_size = item.length - self.prefix.length  # difference in size between item string and prefix
+      item = item[-item_size, item_size]   # remaining item to be inserted into Trie
     end
+    until item.length == 0
+      temp_node.add_child(item[0])
+      prefix.push(item[0])
+      item = item[1..-1]
+      temp_node = temp_node.children.head.value
+    end
+    p prefix
+
     self
   end
 end
